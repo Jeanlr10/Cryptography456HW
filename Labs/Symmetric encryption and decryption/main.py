7dcc8af48d2cb84b2314b8529a1dd870dea9b4b0
JeanLuc R <jlr@robitaille.us>
2024-08-29 14:13:00 -0400

    End of day 8/29/24

--- Labs/Symmetric encryption and decryption/main.py
+++ Labs/Symmetric encryption and decryption/main.py
@@ -1,5 +1,26 @@
-
+#********************************************************#
+#***  _           _       __                          ***#
+#*** | |         | |     /_ |                         ***#
+#*** | |     __ _| |__    | |                         ***#
+#*** | |    / _` | '_ \   | |                         ***#
+#*** | |___| (_| | |_) |  | |                         ***#
+#*** |______\__,_|_.__/   |_|                         ***#
+#*** Lab 1: Symmetric encryption and decryption       ***#
+#*** Due 09/07/2024                                   ***#
+#*** By Jean-Luc Robitaille                           ***#
+#********************************************************#
+#
+#
 import binstr
+message=''
+message_bin=0
+key=''
+key_bin=0
+#
+#
+#
+#
+#
 def xor_cryptography(m_bin,k_bin): # m_bin is the message in binary     k_bin is the key in binary
     key_count=0
     count=0
@@ -15,87 +36,136 @@ def xor_cryptography(m_bin,k_bin): # m_bin is the message in binary     k_bin is
                 output=output+"0"                                       #
             count=count+1
     return output
-keepgoing=True
-message=''
-message_bin=0
-key=''
-key_bin=0
-while keepgoing:
-    print("***************************************************************")
-    print("*                                                             *")
-    print("*       Welcome to Jean-Luc's Simple XOR Cipher Program       *")
-    print("*                                                             *")
-    print("*     Menu:                                                   *")
-    print("*       1: Enter Plaintext Message                            *")
-    print("*       2: View Message in Binary                             *")
-    print("*       3: Enter Encrypted Message                            *")
-    print("*       4: Attempt to View Encrypted Message in ASCII         *")
-    print("*       5: Enter Key                                          *")
-    print("*       6: View Key in Binary                                 *")
-    print("*       7: Encrypt Message                                    *")
-    print("*       8: Decrypt Message                                    *")
-    print("*       9: Exit                                               *")
-    print("*                                                             *")
-    print("***************************************************************")
-    choice=input()
-    try:
-        choice = int(choice)
-    except:
-        choice=0
-    if choice == 1: # Enter Message
-        print("\n\nPlease enter your Plaintext Message:")
-        message=input('')
-        message_bin=binstr.str_to_b(message)
-        #
-        #
-        #
-    elif choice == 2: # View Message as binary
-        if message=='' and message_bin==0:
-            print("\n\nPlease enter your Message:")
-            message=input('')
-        elif message_bin!=0:
-            message=binstr.b_to_str(message_bin)
-        print("\n\nMessage: ",message,"\nMessage Binary: ",message_bin)
-        input('\nPress Enter to continue')
-        #
-        #
-        #
-    elif choice == 3: # Enter Encrypted binary
-        print("\n\nPlease enter your Encrypted Message:")
-        message_bin=input('')
-        message=binstr.b_to_str(message_bin)
-        #
-        #
-        #
-    elif choice == 4: # View Encrypted binary as ASCII
-        if message_bin==0:
-            print("\n\nPlease enter your Encrypted Message:")
-            message_bin=input('')
-            message=binstr.b_to_str(message_bin)
-        print("\nEncrypted Message binary: ",message_bin,"\nEncrypted Message in ASCII: ",message)
-        input('\nPress Enter to continue')
-        #
-        #
-        #
-    elif choice == 5: # Enter Key
-        print("\n\nPlease enter your key:")
-        key=input('')
-        key_bin=binstr.str_to_b(key)
-    elif choice == 6: # View Key as Binary
-        if key=='':
-            print("\n\nPlease enter your Message:")
-            key=input('')
-        print("\nKey: ",key,"\nKey Binary: ",key_bin)
-        input('\nPress Enter to continue')
-    elif choice == 7: # Encrypt Message
-        print("Encoded Message: ",xor_cryptography(message_bin,key_bin))
-        input('\nPress Enter to continue')
-    elif choice == 8: # Decrypt Message
-        print("Decoded Message: ",binstr.b_to_str(xor_cryptography(message_bin,key_bin)))
-        input('\nPress Enter to continue')
-    elif choice == 9: # Exit
-        print("Goodbye!")
-        keepgoing=False
-    else: # Invalid Choice
-        print("Please enter a valid choice")
-        input('\nPress Enter to continue')
+#
+#
+#
+#
+#
+def inputMessage():
+    print("\n\nPlease enter your Plaintext Message:")
+    message=input('')
+    message_bin=binstr.str_to_b(message)
+    return message, message_bin
+#
+#
+#
+#
+#
+def inputEncryptedbin():
+    print("\n\nPlease enter your Encrypted Message:")
+    message_bin=input('')
+    message=binstr.b_to_str(message_bin)
+    return message, message_bin
+#
+#
+#
+#
+#
+def inputkey():
+    print("\n\nPlease enter your key:")
+    key=input('')
+    key_bin=binstr.str_to_b(key)
+    return key,key_bin
+#
+#
+#
+#
+#
+def main():
+    keepgoing=True
+    while keepgoing:
+        print("***************************************************************")
+        print("*                                                             *")
+        print("*       Welcome to Jean-Luc's Simple XOR Cipher Program       *")
+        print("*                                                             *")
+        print("*     Menu:                                                   *")
+        print("*       1: Enter Plaintext Message                            *")
+        print("*       2: View Message in Binary                             *")
+        print("*       3: Enter Encrypted Message                            *")
+        print("*       4: Attempt to View Encrypted Message in ASCII         *")
+        print("*       5: Enter Key                                          *")
+        print("*       6: View Key in Binary                                 *")
+        print("*       7: Encrypt Message                                    *")
+        print("*       8: Decrypt Message                                    *")
+        print("*       9: Exit                                               *")
+        print("*                                                             *")
+        print("***************************************************************")
+        choice=input()
+        try:
+            choice = int(choice)
+        except:
+            choice=0
+        if choice == 1: # Enter Message
+            message,message_bin=inputMessage()
+            #
+            #
+            #
+        elif choice == 2: # View Message as binary
+            #
+            if message=='': # Ensure the message is present
+                message,message_bin=inputMessage()
+            #
+            print("\n\nMessage: ",message,"\nMessage Binary: ",message_bin)
+            input('\nPress Enter to continue')
+            #
+            #
+        elif choice == 3: # Enter Encrypted binary
+            #
+            message,message_bin=inputEncryptedbin()
+            #
+            #
+        elif choice == 4: # View Encrypted binary as ASCII
+            #
+            if message_bin==0: # Ensure the encrypted binary is present
+                message,message_bin=inputEncryptedbin()
+            #
+            print("\nEncrypted Message binary: ",message_bin,"\nEncrypted Message in ASCII: ",message)
+            input('\nPress Enter to continue')
+            #
+            #
+        elif choice == 5: # Enter Key
+            key,key_bin=inputkey()
+            #
+            #
+        elif choice == 6: # View Key as Binary
+            #
+            if key=='': # Ensure the key is present
+                key,key_bin=inputkey()
+            #
+            print("\nKey: ",key,"\nKey Binary: ",key_bin)
+            input('\nPress Enter to continue')
+            #
+            #
+        elif choice == 7: # Encrypt Message
+            #
+            if message_bin==0: # Ensure the message is present
+                message,message_bin=inputMessage()
+            #
+            if key_bin==0: # Ensure the key is present
+                key,key_bin=inputkey()
+            print("Encoded Message: ",xor_cryptography(message_bin,key_bin))
+            input('\nPress Enter to continue')
+            #
+            #
+        elif choice == 8: # Decrypt Message
+            #
+            if message_bin==0:
+                message,message_bin=inputEncryptedbin()
+            if key_bin==0: # Ensure the key is present
+                key,key_bin=inputkey()
+            #
+            print("Decoded Message: ",binstr.b_to_str(xor_cryptography(message_bin,key_bin)))
+            input('\nPress Enter to continue')
+            #
+            #
+        elif choice == 9: # Exit
+            #
+            print("Goodbye!")
+            keepgoing=False
+            #
+            #
+        else: # Invalid Choice
+            #
+            print("Please enter a valid choice")
+            input('\nPress Enter to continue')
+main()
