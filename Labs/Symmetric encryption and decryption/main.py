573a147d18310d296ac2a16959cc77cb580414fb
JeanLuc R <jlr@robitaille.us>
2024-08-29 13:42:47 -0400

    end of class 8/29

--- Labs/Symmetric encryption and decryption/main.py
+++ Labs/Symmetric encryption and decryption/main.py
@@ -1,57 +1,101 @@
-# UNFINISHED
-import binascii
-def strtobin(string):
-    return bin(int(binascii.hexlify(string.encode()), 16)) 
-def bintostr(binary):
-    n = int(binary, 2)
-    return str(binascii.unhexlify('%x' % n))
 
-
-
-plaintext="Hello World"
-key="Iamhere"
-# XOR can be simplified as a!=b
-
-plaintext_bin=str(strtobin(plaintext))
-plaintext_bin=plaintext_bin[3:]
-key_bin=str(strtobin(key))
-key_bin=key_bin[3:]
-
-print("Plaintext is: "+plaintext+"\nPlaintext in binary is: "+plaintext_bin)
-print("Key is: "+key+"\nKey in binary is: "+key_bin)
-
-
-output_encoded =''
-i_bin=0
-i=0
-while i<len(plaintext_bin):
-    i=int(i)
-    if len(key_bin) < i_bin:
-        i_bin+=1
-    else:
-        i_bin=0
-    if bin(int(plaintext_bin[i]))!=bin(int(key_bin[i_bin])):
-        output_encoded=output_encoded+"1"
-    else:
-        output_encoded=output_encoded+"0"
-    i+=1
-print("\n\n\n\n\n\nEncoded:")
-print(output_encoded)
-print(bintostr(output_encoded))
-i_bin=0
-i=0
-output_decoded=''
-while i<len(output_encoded):
-    i=int(i)
-    if len(key_bin) < i_bin:
-        i_bin+=1
-    else:
-        i_bin=0
-    if bin(int(output_encoded[i]))!=bin(int(key_bin[i_bin])):
-        output_decoded=output_decoded+"1"
-    else:
-        output_decoded=output_decoded+"0"
-    i+=1
-print("\n\n\nDecoded:")
-print(output_decoded)
-print(bintostr(output_decoded))
+import binstr
+def xor_cryptography(m_bin,k_bin): # m_bin is the message in binary     k_bin is the key in binary
+    key_count=0
+    count=0
+    output=''
+    while count<len(m_bin):
+            if len(k_bin)-1 <= key_count:       # Ensures the counter does not become greater than the length of the key in binary
+                key_count=0                     #
+            else:                               #
+                key_count=key_count+1           #
+            if bin(int(m_bin[count]))!=bin(int(k_bin[key_count])):      # outputs the logical equivelant of XOR between the 
+                output=output+"1"                                       # current digit of the message binary and the current 
+            else:                                                       # digit of the key binary
+                output=output+"0"                                       #
+            count=count+1
+    return output
+keepgoing=True
+message=''
+message_bin=0
+key=''
+key_bin=0
+while keepgoing:
+    print("***************************************************************")
+    print("*                                                             *")
+    print("*       Welcome to Jean-Luc's Simple XOR Cipher Program       *")
+    print("*                                                             *")
+    print("*     Menu:                                                   *")
+    print("*       1: Enter Plaintext Message                            *")
+    print("*       2: View Message in Binary                             *")
+    print("*       3: Enter Encrypted Message                            *")
+    print("*       4: Attempt to View Encrypted Message in ASCII         *")
+    print("*       5: Enter Key                                          *")
+    print("*       6: View Key in Binary                                 *")
+    print("*       7: Encrypt Message                                    *")
+    print("*       8: Decrypt Message                                    *")
+    print("*       9: Exit                                               *")
+    print("*                                                             *")
+    print("***************************************************************")
+    choice=input()
+    try:
+        choice = int(choice)
+    except:
+        choice=0
+    if choice == 1: # Enter Message
+        print("\n\nPlease enter your Plaintext Message:")
+        message=input('')
+        message_bin=binstr.str_to_b(message)
+        #
+        #
+        #
+    elif choice == 2: # View Message as binary
+        if message=='' and message_bin==0:
+            print("\n\nPlease enter your Message:")
+            message=input('')
+        elif message_bin!=0:
+            message=binstr.b_to_str(message_bin)
+        print("\n\nMessage: ",message,"\nMessage Binary: ",message_bin)
+        input('\nPress Enter to continue')
+        #
+        #
+        #
+    elif choice == 3: # Enter Encrypted binary
+        print("\n\nPlease enter your Encrypted Message:")
+        message_bin=input('')
+        message=binstr.b_to_str(message_bin)
+        #
+        #
+        #
+    elif choice == 4: # View Encrypted binary as ASCII
+        if message_bin==0:
+            print("\n\nPlease enter your Encrypted Message:")
+            message_bin=input('')
+            message=binstr.b_to_str(message_bin)
+        print("\nEncrypted Message binary: ",message_bin,"\nEncrypted Message in ASCII: ",message)
+        input('\nPress Enter to continue')
+        #
+        #
+        #
+    elif choice == 5: # Enter Key
+        print("\n\nPlease enter your key:")
+        key=input('')
+        key_bin=binstr.str_to_b(key)
+    elif choice == 6: # View Key as Binary
+        if key=='':
+            print("\n\nPlease enter your Message:")
+            key=input('')
+        print("\nKey: ",key,"\nKey Binary: ",key_bin)
+        input('\nPress Enter to continue')
+    elif choice == 7: # Encrypt Message
+        print("Encoded Message: ",xor_cryptography(message_bin,key_bin))
+        input('\nPress Enter to continue')
+    elif choice == 8: # Decrypt Message
+        print("Decoded Message: ",binstr.b_to_str(xor_cryptography(message_bin,key_bin)))
+        input('\nPress Enter to continue')
+    elif choice == 9: # Exit
+        print("Goodbye!")
+        keepgoing=False
+    else: # Invalid Choice
+        print("Please enter a valid choice")
+        input('\nPress Enter to continue')
